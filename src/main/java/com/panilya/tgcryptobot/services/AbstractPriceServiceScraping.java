712d45540422d5be24ae4aba6ac9a2b166e0ca2c
panilya <panilyau@gmail.com>
2022-03-31 17:47:08 +0300

    Show changes of price in percents

--- src/main/java/com/panilya/tgcryptobot/services/AbstractPriceServiceScraping.java
+++ src/main/java/com/panilya/tgcryptobot/services/AbstractPriceServiceScraping.java
@@ -2,6 +2,7 @@ package com.panilya.tgcryptobot.services;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
@@ -12,11 +13,34 @@ public abstract class AbstractPriceServiceScraping implements PriceService {
     @Override
     public SendMessage doGetCurrencyPrice(Message message) throws IOException {
         Document doc = Jsoup.connect(doRequest()).get();
+
         String price = doc.select("div.priceValue").text();
-        System.out.println(doc.select("div.priceValue").text());
+        String percentage = doc.select("span[class=sc-15yy2pl-0 gEePkg]").text();
+        System.out.println(price);
+        System.out.println(percentage);
+
+        Elements element = doc.select(".gEePkg span[class^=\"icon\"]");
+
+        final String arrowUp = "\uD83D\uDD3C";
+        final String arrowDown = "\uD83D\uDD3D";
+
+        String holdArrow;
+        if (element.hasClass("icon-Caret-up")) {
+            holdArrow = arrowUp;
+            System.out.println("UP!");
+        } else {
+            holdArrow = arrowDown;
+            System.out.println("DOWN!");
+        }
+
         System.out.println(message.getText()); // Returns KeyboardButton value (BTC, ETH etc.)
 
-        return new MessageCreator().createRawMessage(message, price);
+//        try {
+//            Thread.sleep(50);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return new MessageCreator().createRawMessage(message, price + " " + percentage + holdArrow);
     }
 
     protected abstract String doRequest();
