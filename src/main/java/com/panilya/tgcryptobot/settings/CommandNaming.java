2f5af5df66258cef0b9c9cf833d06f182e1c9450
panilya <panilyau@gmail.com>
2022-05-02 23:13:03 +0300

    Move command's names responsible for main keyboard to separate class as constants

---
+++ src/main/java/com/panilya/tgcryptobot/settings/CommandNaming.java
@@ -0,0 +1,12 @@
+package com.panilya.tgcryptobot.settings;
+
+public final class CommandNaming {
+
+    private CommandNaming() {}
+
+    public static final String START_COMMAND = "/start";
+    public static final String CRYPTOCURRENCY_PRICE = "Cryptocurrency prices";
+    public static final String CONVERT_PRICE = "Currency Exchange";
+    public static final String SHOW_HELP = "Show help";
+
+}
